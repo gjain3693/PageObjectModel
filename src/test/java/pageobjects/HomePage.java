c3e2cb45a96e6532bb8eeb2213def95c85d3e75e
gjain <gaurav.jain1@worldpay.com>
2017-10-09 13:04:43 +0530

    create page object

--- src/test/java/pageobjects/HomePage.java
+++ src/test/java/pageobjects/HomePage.java
@@ -1,5 +1,19 @@
 package pageobjects;
 
-public class HomePage {
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomePage {
+	
+
+	public static WebDriver driver;
+	
+	public static WebElement SIGN_IN = driver.findElement(By.xpath("//*[@id='mc_mainWrapper']/header/div[1]/div/div[2]/div[2]/div[4]/a/strong"));
+	public static WebElement FEEDBACK = driver.findElement(By.xpath("//*[@id='mc_mainWrapper']/header/div[1]/div/div[2]/div[2]/div[3]/a"));
+	public static WebElement STOCKS = driver.findElement(By.xpath("//*[@id='mc_mainWrapper']/header/div[1]/div/div[2]/div[2]/div[2]/a"));
+
+	public static WebElement SEARCH_TXT_BOX = driver.findElement(By.xpath("//*[@id='search_str']"));
 }
